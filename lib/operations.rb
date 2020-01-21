00cb4f0d871df6bf46b5edfee8719314aa2206e6
Justin Pierce <jwpierce1984@gmail.com>
2020-01-21 20:35:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
+  if speed<40 && speed>60
+    return "True"
+    
+  else
+    return "False"
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed<40 && speed>60 ? "True" : "False"
 end
 	
 
