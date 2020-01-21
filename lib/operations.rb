e882dcc46033416ee724437a0794dec2e5241063
Justin Pierce <jwpierce1984@gmail.com>
2020-01-21 20:39:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
-  if speed<40
+  if speed < 40
     true
   
-  elseif speed>60
+  elseif speed > 60
     true
     
   else
@@ -13,7 +13,7 @@ end
 
 
 def not_safe?(speed)
-	speed<40 || speed>60 ? true : false
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
